1f92a7e4deb52f41d2feb8658dfe6981545ef180
Mariusz Klinikowski <nikowek@gmail.com>
2013-11-17 15:02:34 +0100

    Reformat

--- nsendfilebyemail.py
+++ nsendfilebyemail.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import os
 import sys
 from smtplib import SMTP
 from email import encoders
@@ -14,47 +13,47 @@ COMMASPACE = ', '
 
 parser = argparse.ArgumentParser(description='Sending multiple files by multiple e-mails.')
 parser.add_argument('-s', dest='smtp_address',
-    help='SMTP address')
+                    help='SMTP address')
 parser.add_argument('-u', dest='smtp_username',
-    help='SMTP username')
+                    help='SMTP username')
 parser.add_argument('-p', dest='smtp_password',
-    help='SMTP password')
+                    help='SMTP password')
 parser.add_argument('-r', dest='return_address',
-    help='Return address')
+                    help='Return address')
 parser.add_argument('-d', dest='destination_address', action='append',
-    help='Destination address (Can be multiple!)')
+                    help='Destination address (Can be multiple!)')
 #parser.add_argument('--debug', action='store_true',
 #    help='Turn on debug.')
 #parser.add_argument('--gmail', '-g', action='store_true',
 #    help='Turn on GMail mode.')
 parser.add_argument('files', nargs='+',
-    help='Files to transfer')
+                    help='Files to transfer')
 
 arguments = parser.parse_args(sys.argv[1:])
 
 #Sender
 fromaddr = None
-if arguments.return_address==None:
+if arguments.return_address == None:
     print "Insert your e-mail address:"
     fromaddr = sys.stdin.readline()[:-1]#"youremail"
 else:
     fromaddr = arguments.return_address
 
 username = None
-if arguments.smtp_username==None:
+if arguments.smtp_username == None:
     print "Insert your username:"
     username = sys.stdin.readline()[:-1]#"youruser"
 else:
     username = arguments.smtp_username
 
 password = None
-if arguments.smtp_password==None:
+if arguments.smtp_password == None:
     password = getpass("Insert your password:\n")#"yourpassword"
 else:
     password = smtp_password
 
 smtp_addr = None
-if arguments.smtp_address==None:
+if arguments.smtp_address == None:
     print "Insert SMTP adress:"
     smtp_addr = sys.stdin.readline()[:-1]#"smtp_address"
 else:
@@ -62,7 +61,7 @@ else:
 
 #Destination
 destaddr = None
-if arguments.destination_address==None:
+if arguments.destination_address == None:
     print "Insert destination adress:"
     destaddr = sys.stdin.readline()[:-1]#sys.argv[1]
 else:
@@ -70,19 +69,22 @@ else:
 
 print "Data collected."
 
+
 def load_file(filename):
     file_pointer = open(filename, "rb")
     data = file_pointer.read()
     file_pointer.close()
     return data
 
+
 def prepare_attachment(filename):
     data = load_file(filename)
-    attachment = MIMEBase("application","octet-stream")
+    attachment = MIMEBase("application", "octet-stream")
     attachment.set_payload(data)
     encoders.encode_base64(attachment)
     attachment.add_header('Content-Disposition', 'attachment', filename=filename)
     return attachment
+
 
 def prepare_email(fromaddr, recvaddr, filename):
     message = MIMEMultipart()
@@ -94,19 +96,21 @@ def prepare_email(fromaddr, recvaddr, filename):
     message.attach(attachment)
     return message
 
+
 def send_email(message, smtp_address, username, password, tls=True, debug=False):
     server = SMTP(smtp_address)
     server.set_debuglevel(debug)
     if tls:
         server.starttls()
-    server.login(username,password)
+    server.login(username, password)
     server.sendmail(fromaddr, destaddr, message.as_string())
     server.quit()
+
 
 print "Prepared to send messages..."
 
 for filename in arguments.files:
-    print "Sending", filename,": ",
+    print "Sending", filename, ": ",
     sys.stdout.flush()
     message = prepare_email(fromaddr, destaddr, filename)
     send_email(message, smtp_addr, username, password)
